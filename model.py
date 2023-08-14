e909f259adba8421d3b0e3451bf8857f95baf919
JT <jtew396@gmail.com>
2023-08-14 14:15:51 -0600

    Testing logic for what we can allocate

--- model.py
+++ model.py
@@ -6,16 +6,22 @@ from typing import Optional
 @dataclass(frozen=True)
 class OrderLine:
     orderid: str
-    sky: str
+    sku: str
     qty: int
 
 
 class Batch:
+
     def __init__(self, ref: str, sku: str, qty: int, eta: Optional[date]):
         self.reference = ref
         self.sku = sku
         self.eta = eta
         self.available_quantity = qty
 
+
     def allocate(self, line: OrderLine):
         self.available_quantity -= line.qty
+
+
+    def can_allocate(self, line: OrderLine) -> bool:
+        return self.sku == line.sku and self.available_quantity >= line.qty
