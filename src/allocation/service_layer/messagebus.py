95f3a4f2e86ab6425095d7c0aca7a7e17e31e8cc
Jon Thomas Williams <jwilliams@cts1.com>
2024-06-14 10:50:07 -0400

    Add recovery from errors synchronously on handle_event

--- src/allocation/service_layer/messagebus.py
+++ src/allocation/service_layer/messagebus.py
@@ -1,4 +1,5 @@
 import logging
+from tenacity import Retrying, RetryError, stop_after_attempt, wait_exponential
 from typing import Dict, List, Type, Callable, Union
 from allocation.domain import commands, events
 from allocation.service_layer import unit_of_work
@@ -29,11 +30,13 @@ def handle(message: Message, uow: unit_of_work.AbstractUnitOfWork):
 def handle_event(event: events.Event, queue: List[Message], uow: unit_of_work.AbstractUnitOfWork):
     for handler in EVENT_HANDLERS[type(event)]:
         try:
-            logger.debug("handling event %s with handler %s", event, handler)
-            handler(event, uow=uow)
-            queue.extend(uow.collect_new_events())
-        except Exception:
-            logger.exception("Exception handling event %s", event)
+            for attempt in Retrying(stop=stop_after_attempt(3), wait=wait_exponential()):
+                with attempt:
+                    logger.debug("handling event %s with handler %s", event, handler)
+                    handler(event, uow=uow)
+                    queue.extend(uow.collect_new_events())
+        except RetryError as retry_failure:
+            logger.error("Failed to handle event %s times, giving up!", retry_failure.last_attempt.attempt_number)
             continue
 
 
